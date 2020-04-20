446f61a5aff8575e88028c451ca8d1b39c81505c
Niklas Becker <48069565+niklasbec@users.noreply.github.com>
2020-04-20 17:00:03 +0200

    done

--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -1,18 +1,23 @@
 import sys
-sys.path.append('../doubly_linked_list')
+
+sys.path.append('doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
 class Stack:
-    def __init__(self):
-        self.size = 0
-        # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+    
+  def __init__(self):
+    self.size = 0
+    # Why is our DLL a good choice to store our elements?
+    self.storage = []
 
-    def push(self, value):
-        pass
+  def push(self, value):
+    return self.storage.append(value)
 
-    def pop(self):
-        pass
+  def pop(self):
+    if len(self.storage) == 0:
+      return None
+    else: 
+      return self.storage.pop()
 
-    def len(self):
-        pass
+  def len(self):
+    return len(self.storage)
